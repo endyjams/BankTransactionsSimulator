bd90857233e727c8da121501cc04c2125fec2666
endyjams <112718430+endyjams@users.noreply.github.com>
2022-12-15 23:23:22 -0300

    ListTiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,31 @@
 import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: const [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('haha'),
+                subtitle: Text('haha2'),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('haha'),
+                subtitle: Text('haha2'),
+              ),
+            ),
+          ],
+        ),
+        appBar: AppBar(
+          title: const Text("Transactions"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+    ));
